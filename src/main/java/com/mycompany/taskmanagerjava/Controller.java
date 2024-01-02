15ecbb7b1b6abc63cd12d8767639e4f8bf4f645d
NikyRush <155292161+NikyRush@users.noreply.github.com>
2024-01-02 18:09:40 +0300

    Save code in Controller class

--- src/main/java/com/mycompany/taskmanagerjava/Controller.java
+++ src/main/java/com/mycompany/taskmanagerjava/Controller.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.taskmanagerjava;
+
+import Characteristic.CPU;
+import Characteristic.Disk;
+import Characteristic.GPU;
+import Characteristic.RAM;
+import oshi.SystemInfo;
 
 /**
  *
  * @author Admin
  */
 public class Controller {
+    private final CPU cpu;
+    private final RAM ram;
+    private final Disk disk;
+    private final GPU gpu;
+    
+    private static Controller instance;
+    
+    private Controller(SystemInfo si)
+    {
+        cpu = new CPU(si);
+        ram = new RAM(si);
+        disk = new Disk(si);
+        gpu = new GPU(si);
+    }
+    
+    /**
+     * Singltone
+     * @param si - oshi.SystemInfo
+     * @return object Controller (instance)
+     */
+    public static Controller getInstance(SystemInfo si)
+    {
+        if(instance == null)
+            instance = new Controller(si);
+        return instance;
+    }
+
+    public CPU getCpu() { return cpu; }
+
+    public RAM getRam() { return ram; }
+
+    public Disk getDisk() { return disk; }
+
+    public GPU getGpu() { return gpu; }
     
 }
