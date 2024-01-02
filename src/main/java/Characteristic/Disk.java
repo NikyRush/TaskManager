a765147a98be6b42688258f844c9b1eee1a07c12
NikyRush <155292161+NikyRush@users.noreply.github.com>
2024-01-02 16:37:05 +0300

    Add Disk class

---
+++ src/main/java/Characteristic/Disk.java
@@ -0,0 +1,34 @@
+package Characteristic;
+
+import oshi.SystemInfo;
+import oshi.hardware.HWDiskStore;
+import oshi.util.FormatUtil;
+
+/**
+ *
+ * @author Admin
+ */
+public class Disk {
+    private final HWDiskStore diskStore;
+    
+    public Disk(SystemInfo si)
+    {
+        diskStore = si.getHardware().getDiskStores().getFirst();
+    }
+
+    public String getName()
+    {
+        return diskStore.getModel();
+    }
+    
+    public String getTotalSpace()
+    {
+        return FormatUtil.formatBytes(diskStore.getSize());
+    }
+    
+    public String getUsedSpace()
+    {
+        long used = (diskStore.getWriteBytes() + diskStore.getReadBytes()) / 2;
+        return FormatUtil.formatBytes(used);
+    }
+}
