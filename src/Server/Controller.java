8bd8bda572ce4b07272e0b575c9bd101d9722a21
NikyRush <155292161+NikyRush@users.noreply.github.com>
2024-01-27 16:13:43 +0300

    Added a class for client-server operation based on JMX

---
+++ src/Server/Controller.java
@@ -0,0 +1,133 @@
+package Server;
+
+import DataExchange.FunctionMBean;
+import DataExchange.HardwareInfo;
+import DataExchange.LoadInfo;
+import PostgreDB.ManagerDB;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.swing.Timer;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Admin
+ */
+public class Controller {
+    private static Controller instance;
+    private final ManagerDB managerDB;
+    Timer timer;
+    DefaultTableModel modelClients, modelErrors;
+    boolean isActiveServer;
+    
+    int SEC_IN_MIN = 60;
+    int MS_IN_SEC = 1000;
+    
+     /**
+     * Singltone
+     * @return object Controller (instance)
+     */
+    private Controller(DefaultTableModel modelClients, DefaultTableModel modelErrors)
+    {
+        managerDB = new ManagerDB();
+        this.modelClients = modelClients;
+        this.modelErrors = modelErrors;
+    }
+    
+    public static Controller getInstance(DefaultTableModel modelClients, DefaultTableModel modelErrors)
+    {
+        if(instance == null)
+            instance = new Controller(modelClients, modelErrors);
+        return instance;
+    }
+    
+    public void getConnectionDB(String user, String password) throws SQLException
+    {
+        managerDB.getConnection(user, password);
+    }
+    
+    public void ServerStart(int minutes)
+    {       
+        if(timer == null)
+        {
+            timer = new Timer(minutes * MS_IN_SEC * SEC_IN_MIN , e -> {
+                TimerFunction();
+            });
+        }
+        
+        isActiveServer = true;
+        System.out.println("The server is running!");
+        timer.start();
+    }
+    
+    private void TimerFunction()
+    {
+        String ipAddress, port;
+                
+        for(int index = 0; index < modelClients.getRowCount(); index++)
+        {   
+            if(!isActiveServer)
+                break;
+
+            ipAddress = modelClients.getValueAt(index, 1).toString();
+            port = modelClients.getValueAt(index, 2).toString();
+
+            try {
+                managerDB.updateHardwareInfo(getHardwareInfo(ipAddress, port), ipAddress);
+                managerDB.updateLoadInfo(getLoadInfo(ipAddress, port), ipAddress);
+
+            } catch (SQLException | MalformedObjectNameException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                modelErrors.addRow(new Object[] {ipAddress, ex.getLocalizedMessage()});
+            }
+        }
+    }
+    
+    public void ServerStop()
+    {
+        if(timer == null)
+            return;
+        isActiveServer = false;
+        timer.stop();
+        timer = null;
+        System.out.println("The server is stopped!");
+    }
+    
+    private HardwareInfo getHardwareInfo(String ipAddress, String port) throws IOException, MalformedObjectNameException
+    {
+        String conURL = String.format("service:jmx:rmi:///jndi/rmi://%s:%s/jmxrmi", ipAddress.substring(0, ipAddress.length() - 3), port);
+        JMXServiceURL url = new JMXServiceURL(conURL);
+        JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+        MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+        
+        ObjectName name = new ObjectName("DataExchange:type=Function");
+        FunctionMBean mbProxy = JMX.newMBeanProxy(mbsc, name, FunctionMBean.class, false);
+        HardwareInfo hardwareInfo = mbProxy.returnHardwareInfo();
+        jmxc.close();
+        return hardwareInfo;
+    }
+    
+    private LoadInfo getLoadInfo (String ipAddress, String port) throws IOException, MalformedObjectNameException
+    {
+        String conURL = String.format("service:jmx:rmi:///jndi/rmi://%s:%s/jmxrmi", ipAddress.substring(0, ipAddress.length() - 3), port);
+        JMXServiceURL url = new JMXServiceURL(conURL);
+        JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+        MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+        
+        ObjectName name = new ObjectName("DataExchange:type=Function");
+        FunctionMBean mbProxy = JMX.newMBeanProxy(mbsc, name, FunctionMBean.class, false);
+        LoadInfo loadInfo = mbProxy.returnLoadInfo();
+        jmxc.close();
+        return loadInfo;
+    }
+}
